a043e2677fb7d3f2138a4a376fcba9e4b3eb823b
Saruj Chutapornpong <sarujchutapp@gmail.com>
2024-04-18 04:52:59 +0700

    testLogin&Quiz&NewQuiz

---
+++ question/q.js
@@ -0,0 +1,19 @@
+function handleFormSubmit(event) {
+    event.preventDefault();
+    
+    const data = new FormData(event.target);
+    
+    const formJSON = Object.fromEntries(data.entries());
+  
+    // for multi-selects, we need special handling
+    // formJSON.snacks = data.getAll('snacks');
+    
+    const results = document.querySelector('.results pre');
+    results.innerText = JSON.stringify(formJSON, null, 2);
+    question.push(results.innerText)
+    console.log(question)
+  }
+  
+  const form = document.querySelector('.contact-form');
+  form.addEventListener('submit', handleFormSubmit);
+  let question = []

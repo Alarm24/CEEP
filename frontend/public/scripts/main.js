d43258e7371700e937c9eaee7959bc12c52341e1
Nack <nack098gamer@gmail.com>
2024-04-20 05:57:45 +0700

    Fixed The Form Submition Error

--- frontend/public/scripts/main.js
+++ frontend/public/scripts/main.js
@@ -2,8 +2,9 @@ import { createUser } from "./api.js"; // Adjust path if necessary
 
 document.addEventListener("DOMContentLoaded", function () {
   const signUpForm = document.getElementById("signupForm");
-  signUpForm.addEventListener("submit", async function (event) {
-    event.preventDefault();
+  signUpForm.addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const form = document.getElementById("signupForm");
     const username = form.elements["username"].value;
     const password = form.elements["password"].value;
     try {

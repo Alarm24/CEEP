7d05834fab843499148613589cc400703e02eb76
Alarm24 <alarmirl24@gmail.com>
2024-04-20 03:18:23 +0700

    ChangeDirectoryandAddMongo

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
-
-import QuizRoute from "./routes/quizRoute.js";
+import session from "express-session";
+import connectMongoDBSession from "connect-mongodb-session";
 import UserRoute from "./routes/userRoute.js";
 
 const app = express();
+const mongoURI = "mongodb://localhost:27017/sessions";
+const MongoDBStore = connectMongoDBSession(session);
 
 // body-parser
 app.use(express.json());
@@ -13,8 +15,30 @@ app.use(express.urlencoded({ extended: true }));
 // allow request from other origin (Frontend which is at different port)
 app.use(cors());
 
+const store = new MongoDBStore({
+  uri: mongoURI,
+  collection: "mySessions",
+});
+
+app.use(
+  session({
+    secret: "key that will sign cookie",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.get("/", (req, res) => {
+  req.session.isAuth = true;
+  req.session.save((err) => {
+    // Force the session to be saved
+    if (err) console.error("Session save error:", err);
+    res.send("Hello Sessions");
+  });
+});
+
 // use routes
-app.use("/items", QuizRoute);
-app.use("/members", UserRoute);
+app.use("/user", UserRoute);
 
 export default app;
--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,16 +1,30 @@
-// TODO4: you may need to import something here
+import User from "../models/userModel.js";
 
-export const createMember = async (req, res) => {
-  // TODO4: implement this function
-  res.status(501).send("Unimplemented");
+export const createUser = async (req, res) => {
+  try {
+    const existingUser = await User.findOne({ username: req.body.username });
+    if (existingUser) {
+      return res.status(409).send("User already exists.");
+    }
+
+    const newUser = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    await newUser.save();
+    res.status(200).json({ message: "OK" });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).send("Error creating user.");
+  }
 };
 
-export const getMembers = async (req, res) => {
-  // TODO4: implement this function
-  res.status(501).send("Unimplemented");
-};
+// export const getMembers = async (req, res) => {
+//   // TODO4: implement this function
+//   res.status(501).send("Unimplemented");
+// };
 
-export const deleteMember = async (req, res) => {
-  // TODO4: implement this function
-  res.status(501).send("Unimplemented");
-};
+// export const deleteMember = async (req, res) => {
+//   // TODO4: implement this function
+//   res.status(501).send("Unimplemented");
+// };

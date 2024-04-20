f393d555ce35db0fa01604f1baffd6971ffa60d0
Saruj Chutapornpong <sarujchutapp@gmail.com>
2024-04-21 02:27:40 +0700

    linkAllPageTogether&Decorate

--- frontend/public/scripts/newq.js
+++ frontend/public/scripts/newq.js
@@ -14,19 +14,39 @@ function handleFormSubmit(event) {
   console.log(question)
   var doc = document.getElementById("form");
   doc.reset()
+  questionNumber++;
+  document.getElementById("question-number").innerHTML = questionNumber
   document.getElementById("question-name").disabled = true;
   document.getElementById("question-name").style.backgroundColor = "lightgray";
+  if (summit) {
+    document.getElementById("option-modal").style.display = "flex";
+  }
 }
 
-let questionNumber = 0
+let questionNumber = 1
+let summit = false
 
-function nextQuestion(){
-  questionNumber++;
+function load() {
   document.getElementById("question-number").innerHTML = questionNumber
 }
 
-function backToMainPage(){
-  // document.getElementById("question-name").innerHTML
+function canSummit() {
+  summit = true;
+}
+
+function notSummitYet() {
+  summit = false;
+}
+
+function closeOptionModal() {
+  document.querySelector('.results pre').innerText = "";
+  document.getElementById("option-modal").style.display = "none";
+  document.getElementById("question-name").disabled = false;
+  document.getElementById("question-name").style.backgroundColor = "white";
+  document.getElementById("question-name").value = "";
+  questionNumber = 1;
+  document.getElementById("question-number").innerHTML = questionNumber;
+  summit = false;
 }
 
 const form = document.querySelector('.contact-form');

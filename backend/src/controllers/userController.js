c01075b97465ea24961f584d9f9831aed0ab02cd
Alarm24 <alarmirl24@gmail.com>
2024-04-24 06:03:04 +0700

    addGetUser

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,11 @@
 import User from "../models/userModel.js";
 
+export const getUsers = async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json(users);
+};
+
 export const createUser = async (req, res) => {
   try {
     const existingUser = await User.findOne({ username: req.body.username });
--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/register", userController.createUser);
 router.post("/login", userController.loginUser);
 router.post("/update_score", userController.updateScore);
+router.get("/", userController.getUsers);
 
 export default router;
